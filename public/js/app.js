cdaa0a07a570f17cd8c2342bd5edaaa9ed081459
Lukas Sobik <hukash@gmail.com>
2011-03-30 21:21:23 +0200

    peepcode jquery tutorial

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,17 @@
 jQuery(function() {
-  $('ul > li:first').fadeOut(); 
-  $('ul > li:first').fadeIn(); 
+  
+  $("#add").click(function(e) {
+    var taskItem = $('#tasks ul li:first').clone();
+    taskItem.find('form')[0].reset();
+    $("#tasks ul").append(taskItem);
+    taskItem.find('input[type="text"]:first').focus();
+    return false;
+  });
+  
+  $("#add").click().click();
+  
+  $("#tasks ul").sortable({handle: ".handle"}).disableSelection();
+
+  $('input[type="text"]:first').focus();
+
 });
